2964a96578da8e35252a29bf02310de392fb2c02
mltucker <mark.l.tucker@gmail.com>
2016-02-22 12:06:23 +0100

    Sleep and log
    Reboot, then run:
    
    ```bash
    sudo grep plexhometheater /var/log/syslog
    ```

--- plexhometheater/plexhometheater.sh
+++ plexhometheater/plexhometheater.sh
@@ -2,11 +2,16 @@
 export XBMC_HOME=/opt/plexhometheater/share/XBMC
 #Use export AE_ENGINE=SOFT to disable pulse audio
 #export AE_ENGINE=SOFT
-sleep 2
+/usr/bin/logger -s -t plexhometheater.sh "Started"
+sleep 10
 /opt/plexhometheater/bin/plexhometheater &
+/usr/bin/logger -s -t plexhometheater.sh "Ran plexhometheater"
 sleep 2
 #touch ~/plex_started
 wmctrl -x -r plexhometheater.plexhometheater -e 0,0,-1,-1,-1
+/usr/bin/logger -s -t plexhometheater.sh "Ran wmctrl"
 wmctrl -x -r plexhometheater.plexhometheater -b add,fullscreen
+/usr/bin/logger -s -t plexhometheater.sh "Ran wmctrl again"
 sleep 1
 wmctrl -x -r plexhometheater.plexhometheater -b add,fullscreen
+/usr/bin/logger -s -t plexhometheater.sh "Ran wmctrl a third time"
