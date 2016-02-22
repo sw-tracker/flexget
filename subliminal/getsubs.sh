5820fc643df363959d6e88342c976df5b4f098de
sw-tracker <oldiazg@gmail.com>
2016-02-22 11:49:17 +0100

    added logging

--- subliminal/getsubs.sh
+++ subliminal/getsubs.sh
@@ -3,5 +3,6 @@ torrentid=$1
 torrentname=$2
 torrentpath=$3
 
+echo "Torrent Details: " $torrentpath $torrentname $torrentid  > /var/lib/deluge/.subliminal/subliminal.log
 cd $torrentpath
-subliminal download -l en -a /home/deluge/.config/subliminal.cache.dbm .
+subliminal download -l en -a /var/lib/deluge/.subliminal/subliminal.cache.dbm . >> /var/lib/deluge/.subliminal/subliminal.log
