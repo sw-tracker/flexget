cb4a2fb782e47130b075c27469d5578601f332ab
sw-tracker <oldiazg@gmail.com>
2016-02-22 17:30:41 +0100

    fixed cd bug with spaces in path

--- subliminal/getsubs.sh
+++ subliminal/getsubs.sh
@@ -3,6 +3,10 @@ torrentid=$1
 torrentname=$2
 torrentpath=$3
 
+#log input parameters for debugging in case of error
 echo "Torrent Details: " $torrentpath $torrentname $torrentid  > /var/lib/deluge/.subliminal/subliminal.log
-cd $torrentpath
+#move to the folder where the file is located
+cd "$torrentpath"
+#download subtitles for all files in that folder
+#we could use torrentname instead of 'dot' but I personally prefer it this way because I use this script during manual download
 subliminal --cache-dir /var/lib/deluge/.subliminal/ download -l en . >> /var/lib/deluge/.subliminal/subliminal.log
