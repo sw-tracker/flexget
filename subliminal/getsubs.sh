70a655529c68e1a6376ddd4b3eb50cd53654e4c8
sw-tracker <oldiazg@gmail.com>
2016-02-23 21:10:52 +0100

    fix permissions

--- subliminal/getsubs.sh
+++ subliminal/getsubs.sh
@@ -10,3 +10,5 @@ cd "$torrentpath"
 #download subtitles for all files in that folder
 #we could use torrentname instead of 'dot' but I personally prefer it this way because I use this script during manual download
 subliminal --cache-dir /var/lib/deluge/.subliminal/ download -l en . >> /var/lib/deluge/.subliminal/subliminal.log
+#fix permissions on new folder and files so plex can see them
+chmod -R u=rwx,g=rwx,o=rx "$torrentpath" >> /var/lib/deluge/.subliminal/subliminal.log
