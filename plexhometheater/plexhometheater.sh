2293426a8cb012b50a77c9fdbbd711c25b2160ab
sw-tracker <oldiazg@gmail.com>
2016-02-24 09:00:38 +0100

    remove touch

--- plexhometheater/plexhometheater.sh
+++ plexhometheater/plexhometheater.sh
@@ -5,7 +5,6 @@ export XBMC_HOME=/opt/plexhometheater/share/XBMC
 sleep 2
 /opt/plexhometheater/bin/plexhometheater &
 sleep 2
-#touch ~/plex_started
 wmctrl -x -r plexhometheater.plexhometheater -e 0,0,-1,-1,-1
 wmctrl -x -r plexhometheater.plexhometheater -b add,fullscreen
 sleep 1
